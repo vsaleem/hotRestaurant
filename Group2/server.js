e5b125e1ef435dc5626cb25da7c34c278546de08
Alex Taylor <altays633@gmail.com>
2020-04-02 01:50:44 -0400

    uncommenting api and html routes

--- Group2/server.js
+++ Group2/server.js
@@ -29,10 +29,10 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 // require htmlRoutes, too, using the same pattern
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 // =============================================================================
